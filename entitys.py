56721384fa3868cbc45c1041d1ddbdc4fe6b5ed5
kayosman <30638048+kayosman@users.noreply.github.com>
2022-03-12 00:48:30 -0500

    Started collecting milk

---
+++ entitys.py
@@ -0,0 +1,15 @@
+class Entity:
+	def __init__(self, name)
+	self.name = name
+
+	def changeName(self, newName):
+		print("            		/", 
+			  "   (___)--------|/ ",  
+			  "   (o o)       ||  ",  
+			  " YY ( )-||--I--||  ",  
+			  " ||--I--||-( ) ^^  ",  
+			  " ||     ^^(o o)--< ",  
+			  "/|--------(---)--< ",  
+			 "/                   ",  
+			"* 6ixte9ine to 69 \n ")
+		self.name = newName
