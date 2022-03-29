d78968375e2fcf036ad544456421a363d5f4b65f
trent <21tredman@masdblueraiders.org>
2022-03-29 04:12:20 -0400

    Broken Build

--- scripts/main.py
+++ scripts/main.py
@@ -8,11 +8,4 @@ def main():
     pass
 
 if __name__ == '__main__':
-    player = Player("Bob", True, 100, 1, 0)
-    print(player.level)
-    player.exp = 100000
-    player.levelUp()
-    print(player.level)
-    sword = Sword("Sword", 1, 1)
-    sword.generateStats()
-    print(sword)
+    main()
